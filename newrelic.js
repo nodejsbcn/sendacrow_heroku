67af5dce9aff997ed5f24df22f12a23d026662c7
Jesus Seijas <jseijas@gmail.com>
2016-05-16 23:57:21 +0200

    Check newrelic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Sendacrow'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '83ef0ca3ddd074d5d8aa8d6d080fa9c974759d78',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,27 +1,49 @@
+var newrelic = require('newrelic');
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var https = require('https');
+var http = require('http');
+var sslConfig = require('./private/ssl-config');
+var httpsRedirect = require('./middleware/https-redirect');
 
 var app = module.exports = loopback();
+app.use(httpsRedirect(app));
 
+
+function showExplorerUrl(baseUrl) {
+  if (app.get('loopback-component-explorer')) {
+    console.log('Browse your REST API at %s%s', baseUrl, 
+      app.get('loopback-component-explorer').mountPath);
+  }
+} 
+
+  
+  
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+  var httpOnly = process.env.NODE_HTTP_ONLY || false;
+  var httpsPort = app.get('https-port');
+  var port = app.get('port');
+  var host = app.get('host');
+  http.createServer(app).listen(port, function() {
+    var httpUrl = 'http://'+host+':'+port;
+    if (httpOnly) {
+      app.emit('started');
+      console.log('Loopback server listening @ %s%s', httpUrl, '/');
+      showExplorerUrl(httpUrl);
+    } else {
+      var httpsUrl = 'https://'+host+':'+httpsPort;
+      https.createServer(sslConfig.credentials, app).listen(httpsPort, function() {
+        app.emit('started');
+        console.log('Loopback server redirecting @ %s%s', httpUrl, '/');
+        console.log('Loopback server listening @ %s%s', httpsUrl, '/');
+        showExplorerUrl(httpsUrl);
+      })
+    }   
+  });  
+}
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
-
-  // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
